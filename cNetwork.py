2926bf59856bdd78c77bbac0cbb52d3047f07cca
Bolei Zhou <zhoubolei@gmail.com>
2013-11-10 17:52:37 -0500

    Create cNetwork.py

---
+++ cNetwork.py
@@ -0,0 +1,304 @@
+"""
+building convolutional network on facial emotion dataset
+
+Bolei Zhou
+
+usage: test it on GPU
+THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python convolutional_bolei.py
+
+"""
+#TODO: 
+# 1) the module of IO for loading and saving model parameters.
+# 2) the module of testing the best model on testing dataset.
+# 3) the module of plotting error samples.
+
+import cPickle
+import os
+import sys
+import time
+
+import csv
+
+import numpy as np
+
+import theano
+import theano.tensor as T
+
+
+from theano.tensor.signal import downsample
+from theano.tensor.nnet import conv
+
+from logistic_sgd import LogisticRegression
+from mlp import HiddenLayer
+from convolutional_mlp import LeNetConvPoolLayer
+
+class Dataset(object):
+    def __init__(self):
+        
+        #self.base_path = os.environ['PYLEARN2_DATA_PATH'] + '/icml_2013_emotions/'
+        self.base_path = '/home/bolei/data/icml_2013_emotions/'
+        self.files = {'train': 'train.csv', 'test' : 'test.csv'}
+        self.validationSetRatio = 0.2 # the size of validation set
+
+    def loadTrain(self):    
+        print '... loading training data'
+        expect_labels = 1
+        setType = 'train'
+        
+        csv_file = open(self.base_path + self.files[setType], 'r')
+        reader = csv.reader(csv_file)
+        
+        # Discard header
+        row = reader.next()
+
+        y_list = []
+        X_list = []
+
+        for row in reader:
+            if expect_labels:
+                y_str, X_row_str = row
+                y = int(y_str)
+                y_list.append(y)
+            else:
+                X_row_str ,= row
+            X_row_strs = X_row_str.split(' ')
+            X_row = map(lambda x: float(x), X_row_strs)
+            X_list.append(self.histeq(X_row))    
+     
+        print '... randomly separting training set and validation set' 
+
+        sizeTrainSet = len(X_list)        
+        startIndex_validation = int(self.validationSetRatio*sizeTrainSet)
+        randomIndex = np.random.permutation([i for i in range(sizeTrainSet)])
+        
+
+        X_list_validation = [X_list[i] for i in randomIndex[:startIndex_validation]]
+        y_list_validation = [y_list[i] for i in randomIndex[:startIndex_validation]]    
+            
+        
+        X_list_train = [X_list[i] for i in randomIndex[startIndex_validation:]]
+        y_list_train = [y_list[i] for i in randomIndex[startIndex_validation:]]   
+        
+        del X_list
+        train_set_x, train_set_y = self.shared_dataset(X_list_train, y_list_train)
+        valid_set_x, valid_set_y = self.shared_dataset(X_list_validation, y_list_validation)
+        
+        rval = [(train_set_x, train_set_y), (valid_set_x, valid_set_y)]
+        return rval
+
+        
+    def shared_dataset(self, data_x, data_y, borrow=True):
+        """ Function that loads the dataset into shared variables
+
+        """
+
+        shared_x = theano.shared(np.asarray(data_x,
+                                               dtype=theano.config.floatX),
+                                 borrow=borrow)
+        shared_y = theano.shared(np.asarray(data_y,
+                                               dtype=theano.config.floatX),
+                                 borrow=borrow)
+
+        return shared_x, T.cast(shared_y, 'int32')  
+        
+    def histeq(self, face_vector,nbr_bins=256):
+      ## histogram normalization for the face images.
+      # get image histogram
+        imhist,bins = np.histogram(face_vector,nbr_bins,normed=True)
+        cdf = imhist.cumsum() # cumulative distribution function
+        cdf = 255 * cdf / cdf[-1] # normalize
+
+      # use linear interpolation of cdf to find new pixel values
+        face_vector_normalized = np.interp(face_vector,bins[:-1],cdf)
+
+        return face_vector_normalized
+
+
+def runDeepLearning():
+### LOADING TRAINING SET
+    myDataset = Dataset()
+    datasets = myDataset.loadTrain()
+    train_set_x, train_set_y = datasets[0]
+    valid_set_x, valid_set_y = datasets[1]
+    print train_set_y
+    
+### Model parameters
+    learning_rate = 0.02
+    n_epochs = 2000
+    nkerns=[30, 40, 40] # number of kernal at each layer, current best performance is 50.0% on testing set, kernal number is [30,40,40]
+    batch_size = 800
+    
+    # compute number of minibatches for training, validation and testing
+    n_train_batches = train_set_x.get_value(borrow=True).shape[0]
+    n_valid_batches = valid_set_x.get_value(borrow=True).shape[0]
+    n_train_batches /= batch_size
+    n_valid_batches /= batch_size
+
+
+    # allocate symbolic variables for the data
+    index = T.lscalar()  # index to a [mini]batch
+    x = T.matrix('x')   # the data is presented as rasterized images
+    y = T.ivector('y')  # the labels are presented as 1D vector of
+                        # [int] labels
+
+
+    ishape = (48, 48)  # size of input images
+    nClass = 7
+    
+    rng = np.random.RandomState(23455)
+    ######################
+    # BUILD ACTUAL MODEL #
+    ######################
+    print '... building the model'
+
+    # Reshape matrix of rasterized images of shape (batch_size,28*28)
+    # to a 4D tensor, compatible with our LeNetConvPoolLayer
+    layer0_input = x.reshape((batch_size, 1, ishape[0], ishape[0]))
+
+    # Construct the first convolutional pooling layer:
+    # filtering reduces the image size to (28-5+1,28-5+1)=(24,24)
+    # maxpooling reduces this further to (24/2,24/2) = (12,12)
+    # 4D output tensor is thus of shape (batch_size,nkerns[0],12,12)
+    layer0 = LeNetConvPoolLayer(rng, input=layer0_input,
+            image_shape=(batch_size, 1, ishape[0], ishape[0]),
+            filter_shape=(nkerns[0], 1, 5, 5), poolsize=(2, 2))
+
+    # Construct the second convolutional pooling layer
+    # filtering reduces the image size to (12-5+1,12-5+1)=(8,8)
+    # maxpooling reduces this further to (8/2,8/2) = (4,4)
+    # 4D output tensor is thus of shape (nkerns[0],nkerns[1],4,4)
+    layer1 = LeNetConvPoolLayer(rng, input=layer0.output,
+            image_shape=(batch_size, nkerns[0], 22, 22),
+            filter_shape=(nkerns[1], nkerns[0], 5, 5), poolsize=(2, 2))
+
+    layer2 = LeNetConvPoolLayer(rng, input=layer1.output,
+            image_shape=(nkerns[0], nkerns[1], 9, 9),
+            filter_shape=(nkerns[2], nkerns[1], 2, 2), poolsize=(2, 2))    
+
+    # the TanhLayer being fully-connected, it operates on 2D matrices of
+    # shape (batch_size,num_pixels) (i.e matrix of rasterized images).
+    # This will generate a matrix of shape (20,32*4*4) = (20,512)
+    layer3_input = layer2.output.flatten(2)
+
+    # construct a fully-connected sigmoidal layer
+    layer3 = HiddenLayer(rng, input=layer3_input, n_in=nkerns[2] * 4 * 4,
+                         n_out=500, activation=T.tanh)
+
+    # classify the values of the fully-connected sigmoidal layer
+    layer4 = LogisticRegression(input=layer3.output, n_in=500, n_out=nClass)
+
+    # the cost we minimize during training is the NLL of the model
+    cost = layer4.negative_log_likelihood(y)
+
+    # create a function to compute the mistakes that are made by the model
+
+    validate_model = theano.function([index], layer4.errors(y),
+            givens={
+                x: valid_set_x[index * batch_size: (index + 1) * batch_size],
+                y: valid_set_y[index * batch_size: (index + 1) * batch_size]})
+
+    # create a list of all model parameters to be fit by gradient descent
+    params = layer4.params + layer3.params + layer2.params + layer1.params + layer0.params
+
+    # create a list of gradients for all model parameters
+    grads = T.grad(cost, params)
+
+    # train_model is a function that updates the model parameters by
+    # SGD Since this model has many parameters, it would be tedious to
+    # manually create an update rule for each model parameter. We thus
+    # create the updates list by automatically looping over all
+    # (params[i],grads[i]) pairs.
+    updates = []
+    for param_i, grad_i in zip(params, grads):
+        updates.append((param_i, param_i - learning_rate * grad_i))
+
+    train_model = theano.function([index], cost, updates=updates,
+          givens={
+            x: train_set_x[index * batch_size: (index + 1) * batch_size],
+            y: train_set_y[index * batch_size: (index + 1) * batch_size]})
+
+    ###############
+    # TRAIN MODEL #
+    ###############
+    print '... training'
+    # early-stopping parameters
+    patience = 10000  # look as this many examples regardless
+    patience_increase = 2  # wait this much longer when a new best is
+                           # found
+    improvement_threshold = 0.995  # a relative improvement of this much is
+                                   # considered significant
+    validation_frequency = min(n_train_batches, patience / 2)
+                                  # go through this many
+                                  # minibatche before checking the network
+                                  # on the validation set; in this case we
+                                  # check every epoch
+
+    best_params = None
+    best_validation_loss = np.inf
+    best_iter = 0
+    test_score = 0.
+    start_time = time.clock()
+
+    epoch = 0
+    done_looping = False
+
+    while (epoch < n_epochs) and (not done_looping):
+        epoch = epoch + 1
+        for minibatch_index in xrange(n_train_batches):
+
+            iter = (epoch - 1) * n_train_batches + minibatch_index
+
+            if iter % 100 == 0:
+                print 'training @ iter = ', iter
+            cost_ij = train_model(minibatch_index)
+
+            if (iter + 1) % validation_frequency == 0:
+
+                # compute zero-one loss on validation set
+                validation_losses = [validate_model(i) for i
+                                     in xrange(n_valid_batches)]
+                this_validation_loss = np.mean(validation_losses)
+                print('epoch %i, minibatch %i/%i, validation error %f %%' % \
+                      (epoch, minibatch_index + 1, n_train_batches, \
+                       this_validation_loss * 100.))
+
+                # if we got the best validation score until now
+                if this_validation_loss < best_validation_loss:
+
+                    #improve patience if loss improvement is good enough
+                    if this_validation_loss < best_validation_loss *  \
+                       improvement_threshold:
+                        patience = max(patience, iter * patience_increase)
+
+                    # save best validation score and iteration number
+                    best_validation_loss = this_validation_loss
+                    best_iter = iter
+
+                    # test it on the test set
+                    """
+                    test_losses = [test_model(i) for i in xrange(n_test_batches)]
+                    test_score = np.mean(test_losses)
+                    print(('     epoch %i, minibatch %i/%i, test error of best '
+                           'model %f %%') %
+                          (epoch, minibatch_index + 1, n_train_batches,
+                           test_score * 100.))
+                    """
+            if patience <= iter:
+                done_looping = True
+                break
+
+    end_time = time.clock()
+    print('Optimization complete.')
+    
+    #TODO: write the code to save the trained model and test the trained model on test data
+    
+    print('Best validation score of %f %% obtained at iteration %i,'\
+          'with test performance %f %%' %
+          (best_validation_loss * 100., best_iter + 1, 0))
+    print >> sys.stderr, ('The code for file ' +
+                          os.path.split(__file__)[1] +
+                          ' ran for %.2fm' % ((end_time - start_time) / 60.))
+
+if __name__ == '__main__':
+    runDeepLearning()
+
