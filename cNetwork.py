74f3ceaf01e283b438d40b7b3b93d84628b0d3ad
Bolei Zhou <bzhou@localhost>
2013-11-11 18:33:29 -0500

    add plot function in dataset object

--- cNetwork.py
+++ cNetwork.py
@@ -153,11 +153,47 @@ class Dataset(object):
 
         return face_vector_normalized
 
-    def plotExample(self, dataList, sampleIndex, outputNum = 10000):
+    def plotExample(self, dataList, labelList,  sampleIndex, outputNum = 10000):
+        #TODO
         # plotting out the samples according to the sample Index
         # dataList = [[3,4,5,5..],[4,5,6,7]...,...] 
         # sampleIndex = [13,4,5,
         # output a image with [sqrt(otuputNum), sqrt(outputNum)]
+        """
+        Hint: 
+        blank_image = Image.new("RGB", (800, 600))
+        blank_image.paste(image64, (0,0))
+        blank_image.paste(fluid128, (400,0))
+        blank_image.paste(fluid512, (0,300))
+        blank_image.paste(fluid1024, (400,300))
+        blank_image.save(out)
+        
+        
+        import Image
+
+        #opens an image:
+        im = Image.open("1_tree.jpg")
+        #creates a new empty image, RGB mode, and size 400 by 400.
+        new_im = Image.new('RGB', (400,400))
+
+        #Here I resize my opened image, so it is no bigger than 100,100
+        im.thumbnail((100,100))
+        #Iterate through a 4 by 4 grid with 100 spacing, to place my image
+        for i in xrange(0,500,100):
+            for j in xrange(0,500,100):
+                #I change brightness of the images, just to emphasise they are unique copies.
+                im=Image.eval(im,lambda x: x+(i+j)/30)
+                #paste the image at location i,j:
+                new_im.paste(im, (i,j))
+
+        new_im.show()
+        """
+        sampleList = [dataList[i] for i in sampleIndex]
+        
+        imgVector = sampleList[0]
+        singleImg = np.asarray(imgVector).astype('uint8').reshape((48,48))
+        im = Image.fromarray(singleImg)
+        im.save('myImg.jpg')
         pass
         
 def runDeepLearning():
