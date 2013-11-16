7722a606bb29c1f5b79f173967cdbfeeb8d6f3d5
Bolei Zhou <bzhou@localhost>
2013-11-16 11:48:25 -0500

    added more output in def errors

--- logistic_sgd.py
+++ logistic_sgd.py
@@ -142,6 +142,31 @@ class LogisticRegression(object):
             return T.mean(T.neq(self.y_pred, y))
         else:
             raise NotImplementedError()
+            
+    def errorsLabel(self, y):
+        """Return a float representing the number of errors in the minibatch
+        over the total number of examples of the minibatch ; zero one
+        loss over the size of the minibatch
+
+        :type y: theano.tensor.TensorType
+        :param y: corresponds to a vector that gives for each example the
+                  correct label
+        """
+
+        # check if y has same dimension of y_pred
+        if y.ndim != self.y_pred.ndim:
+            raise TypeError('y should have the same shape as self.y_pred',
+                ('y', target.type, 'y_pred', self.y_pred.type))
+        # check if y is of the correct datatype
+        if y.dtype.startswith('int'):
+            # the T.neq operator returns a vector of 0s and 1s, where 1
+            # represents a mistake in prediction
+            return (T.mean(T.neq(self.y_pred, y)), self.y_pred, y)
+        else:
+            raise NotImplementedError()
+
+    def outputLabel(self):
+        return self.y_pred
 
 
 def load_data(dataset):
